7b17470a94689408b67dace138001c12879cee16
Erik Hollensbe <github@hollensbe.org>
2019-04-19 12:45:39 +0000

    Fixups for latest ci-agents patches:
    * Bring in latest ci-agents repo
    * Remove all references to oauth2 tokens (use our types lib)
    * Lots of logging
    * Finally, validate the configuration file which will populate it with
      the default values
    
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- runner/git.go
+++ runner/git.go
@@ -4,8 +4,9 @@ import (
 	"encoding/json"
 	"io"
 
+	"github.com/tinyci/ci-agents/clients/log"
+	"github.com/tinyci/ci-agents/types"
 	"github.com/tinyci/ci-runners/git"
-	"golang.org/x/oauth2"
 )
 
 func jsonIO(from, to interface{}) error {
@@ -18,35 +19,50 @@ func jsonIO(from, to interface{}) error {
 }
 
 // PullRepo retrieves the repository and puts it in the right spot.
-func (r *Run) PullRepo(log io.Writer) (*git.RepoManager, error) {
-	queueTok := r.QueueItem.Run.Task.Parent.Owners[0].Token
-	tok := &oauth2.Token{}
+func (r *Run) PullRepo(w io.Writer) (*git.RepoManager, error) {
+	queueTok := r.QueueItem.Run.Task.Parent.Owner.Token
+	tok := &types.OAuthToken{}
 
 	if err := jsonIO(queueTok, tok); err != nil {
 		return nil, err
 	}
 
 	rm := &git.RepoManager{
-		Log:          log,
-		AccessToken:  tok.AccessToken,
+		Log:          w,
+		AccessToken:  tok.Token,
 		BaseRepoPath: r.Config.Runner.BaseRepoPath,
 	}
 
+	wf := r.Config.Clients.Log.WithFields(log.FieldMap{
+		"owner":          r.QueueItem.Run.Task.Parent.Owner.Username,
+		"base_repo_path": r.Config.Runner.BaseRepoPath,
+		"repo_name":      r.QueueItem.Run.Task.Parent.Name,
+	})
+
 	if err := rm.Init(r.Config, r.QueueItem.Run.Task.Parent.Name, r.QueueItem.Run.Task.Ref.Repository.Name); err != nil {
+		wf.Errorf("Error initializing repo: %v", err)
 		return nil, err
 	}
 
 	if err := rm.CloneOrFetch(); err != nil {
+		wf.Errorf("Error cloning repo: %v", err)
 		return nil, err
 	}
 
 	if err := rm.AddOrFetchFork(); err != nil {
+		wf.Errorf("Error cloning fork: %v", err)
 		return nil, err
 	}
 
 	if err := rm.Checkout(r.QueueItem.Run.Task.Ref.SHA); err != nil {
+		wf.Errorf("Error checking out %v: %v", r.QueueItem.Run.Task.Ref.SHA, err)
 		return nil, err
 	}
 
-	return rm, rm.Merge("origin/master")
+	if err := rm.Merge("origin/master"); err != nil {
+		wf.Errorf("Error merging master for %v: %v", r.QueueItem.Run.Task.Ref.SHA, err)
+		return nil, err
+	}
+
+	return rm, nil
 }
