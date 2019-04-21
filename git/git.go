03782b0374de54394cd1266c36db85e2ff2c6d9d
Erik Hollensbe <github@hollensbe.org>
2019-04-21 03:10:32 -0700

    Move git configuration to own go file
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- git/git.go
+++ git/git.go
@@ -14,40 +14,6 @@ import (
 	"github.com/tinyci/ci-agents/errors"
 )
 
-const (
-	defaultLoginScriptPath = "/tmp/tinyci-github-login.sh"
-	defaultBaseRepoPath    = "/tmp/git"
-	defaultGitUserName     = "tinyCI runner"
-	defaultGitEmail        = "no-reply@example.org"
-)
-
-// Config manages various one-off tidbits about the runner's git paths and other data.
-type Config struct {
-	LoginScriptPath string `yaml:"login_script_path"`
-	BaseRepoPath    string `yaml:"base_repo_path"`
-}
-
-// Validate corrects or errors out when the configuration doesn't match expectations.
-func (rc *Config) Validate() error {
-	if rc.LoginScriptPath == "" {
-		rc.LoginScriptPath = defaultLoginScriptPath
-	}
-
-	if !filepath.IsAbs(rc.LoginScriptPath) {
-		return errors.New("login_script_path must be absolute")
-	}
-
-	if rc.BaseRepoPath == "" {
-		rc.BaseRepoPath = defaultBaseRepoPath
-	}
-
-	if !filepath.IsAbs(rc.BaseRepoPath) {
-		return errors.New("base_repo_path must be absolute")
-	}
-
-	return nil
-}
-
 // RepoManager manages a series of repositories.
 type RepoManager struct {
 	Config       Config
