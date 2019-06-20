3b57b9fa8d31047a03246cfa64176fa1c90ee4ea
Erik Hollensbe <github@hollensbe.org>
2019-06-20 03:28:33 -0700

    Update/Correct description of null runner commandline
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- cmd/null-runner/main.go
+++ cmd/null-runner/main.go
@@ -10,8 +10,7 @@ func main() {
 	err := fw.Run(fw.Entrypoint{
 		Usage: "Run tinyci jobs with overlayfs and docker",
 		Description: `
-This runner provides a docker interface to running tinyci builds. It also
-leverages an overlayfs backend and git cache to make clones fast.
+This runner mocks a real runner and provides no function but to report statuses.
 `,
 		Launch:          &runner.Runner{},
 		TeardownTimeout: 0,
