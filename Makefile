885778943206d8e5f238954d1380d442da102471
Erik Hollensbe <github@hollensbe.org>
2021-01-21 21:15:16 -0800

    Support building in container for overlay-runner
    This allows the overlay-runner to function with an embedded dockerd.
    
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-GO_VERSION ?= 1.15
+GO_VERSION ?= 1.15-alpine
 
 all:
 	go install -v ./...
--- box-builds/box-dist.rb
+++ box-builds/box-dist.rb
@@ -1,7 +1,12 @@
-from "ubuntu:20.04"
+from "docker:dind"
 
 after { flatten }
 
-run "apt-get update && apt-get install git -y && apt-get clean"
-skip { copy "release.tar.gz", "/" }
-run "tar -vxz -C /usr/bin --strip-components=1 -f release.tar.gz"
+run "apk add git"
+copy "release.tar.gz", "/tmp/release.tar.gz"
+run "cd /tmp && tar vxzf release.tar.gz && chown -R root:root /tmp/build && mv /tmp/build/* /usr/local/bin && rm -r /tmp/release.tar.gz /tmp/build"
+
+copy "entrypoint.sh", "/entrypoint.sh"
+run "chmod 755 /entrypoint.sh"
+
+entrypoint "/entrypoint.sh"
