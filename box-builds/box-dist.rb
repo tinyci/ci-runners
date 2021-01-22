61bd8c4e0e74774c45254dd424be829514a43f20
Erik Hollensbe <github@hollensbe.org>
2021-01-21 19:04:16 -0800

    add git to box image
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- box-builds/box-dist.rb
+++ box-builds/box-dist.rb
@@ -2,5 +2,6 @@ from "ubuntu:20.04"
 
 after { flatten }
 
+run "apt-get update && apt-get install git -y && apt-get clean"
 skip { copy "release.tar.gz", "/" }
 run "tar -vxz -C /usr/bin --strip-components=1 -f release.tar.gz"
